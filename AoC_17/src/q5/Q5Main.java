1defe7bfd6136e33d484f5f55a97376b0abcd130
LucasSherlock <lshe485@aucklanduni.ac.nz>
2017-12-25 20:24:27 +1300

    finshed code for Q5

--- AoC_17/src/q5/Q5Main.java
+++ AoC_17/src/q5/Q5Main.java
@@ -1,10 +1,66 @@
 package q5;
 
+import java.util.ArrayList;
+
 public class Q5Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	private static ArrayList<String> input = q1.Main.readFile("Q5_Input.txt");
+	public static void main(String[] args) {
+		ptOne();
+		ptTwo();
 	}
+	
+	public static void ptOne() {
+		int[] jumps = new int[input.size()];
+		int i = 0;
+		for(String s : input) {
+			jumps[i] = Integer.parseInt(s);
+			i++;
+		}
+
+		int steps = 0;
+		try {
+			i = 0;
+			while(true) {
+				jumps[i] += 1;
+				i += jumps[i]-1;
+				
+				steps++;
+			}
+		} catch(ArrayIndexOutOfBoundsException ex) {
+			System.out.println(steps);
+		}
+	}
+	
+	public static void ptTwo() {
+		int[] jumps = new int[input.size()];
+		int i = 0;
+		for(String s : input) {
+			jumps[i] = Integer.parseInt(s);
+			i++;
+		}
+
+		int steps = 0;
+		try {
+			i = 0;
+			while(true) {
+				
+				if(jumps[i] >= 3) { 
+					jumps[i] -= 1;
+					i += jumps[i]+1;
+				} else {
+					jumps[i] += 1;
+					i += jumps[i]-1;
+				}
+					
+				steps++;
+			}
+		} catch(ArrayIndexOutOfBoundsException ex) {
+			System.out.println(steps);
+		}
+	}
+	
+	
 
 }
