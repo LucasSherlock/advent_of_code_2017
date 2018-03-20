761f7efde100acd5560d01ba2f324eb9be5713fd
LucasSherlock <lshe485@aucklanduni.ac.nz>
2018-03-20 15:59:38 +1300

    added code for q8 part 1

--- AoC_17/src/q8/Main.java
+++ AoC_17/src/q8/Main.java
@@ -1,25 +1,93 @@
 package q8;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Main {
-
-	private static ArrayList<String> input = q1.Main.readFile("Q8_InputTest.txt");
+	//The question input
+	private static ArrayList<String> input = q1.Main.readFile("Q8_Input.txt");
+	
+	// A hashmap to list all the registers.
+	protected static HashMap<String, Integer> regs = new HashMap<String, Integer>();
 	
 	public static void main(String[] args) {
+		ptOne();
+	
+
+	}
+	
+	private static void ptOne() {
+		populateRegs();
+		
+		for(String s : input) {
+			doOperation(s);	
+		}
+		int max = regs.get(input.get(0).split(" ")[0]);
+		for(int i : regs.values()) {
+			if(i > max) {
+				max = i;
+			}
+		}
+		System.out.println(max);
+	}
+	
+	private static void populateRegs() {
 		String[] line;
-		ArrayList<Register> regs = new ArrayList<Register>();
 		HashSet<String> names = new HashSet<String>();
 		for(String s : input) {
 			line = s.split(" ");
-			names.add(line[0]);
+			names.add(line[0]);	
+			names.add(line[4]);	
 		}
 		 for(String name : names) {
-			 regs.add(new Register(name));
+			 regs.put(name, 0);
 		 }
+	}
 	
+	private static void doOperation(String s) {
+		String[] line = s.split(" ");
+		boolean condition = false;
+		switch(line[5]) {
+		
+		case "<": if(regs.get(line[4]) < Integer.parseInt(line[6])) {
+			condition = true;
+		}
+		break;
+		
+		case ">": if(regs.get(line[4]) > Integer.parseInt(line[6])) {
+			condition = true;
+		}
+		break;
+		
+		case "<=": if(regs.get(line[4]) <= Integer.parseInt(line[6])) {
+			condition = true;
+		}
+		break;
+		
+		case ">=": if(regs.get(line[4]) >= Integer.parseInt(line[6])) {
+			condition = true;
+		}
+		break;
+		case "==": if(regs.get(line[4]) == Integer.parseInt(line[6])) {
+			condition = true;
+		}
+		break;
+		
+		case "!=": if(regs.get(line[4]) != Integer.parseInt(line[6])) {
+			condition = true;
+		}
+		break;
 
+		}
+		
+		if(condition) {
+			if(line[1].equals("inc")) {
+				regs.put(line[0], regs.get(line[0])+Integer.parseInt(line[2]));
+			} else {
+				regs.put(line[0], regs.get(line[0])-Integer.parseInt(line[2]));
+			}
+		}
 	}
 
 }
